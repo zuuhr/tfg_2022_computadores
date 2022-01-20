6a94e943b7397aaccb4d273dc5715e2454d6a045
zuuhr <43469859+zuuhr@users.noreply.github.com>
2022-01-20 12:33:15 +0100

    SSAO 0.1v

--- src/scenes/normalScene.ts
+++ src/scenes/normalScene.ts
@@ -12,6 +12,7 @@ export class NormalScene implements CreateSceneClass {
         var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(29, 13, 23), scene);
         camera.setTarget(new BABYLON.Vector3(0, 0, 0));
         camera.attachControl(canvas);
+        camera.maxZ = 200; //Tweak to see better xd
 
         // Create some boxes and deactivate lighting (specular color and back faces)
         var boxMaterial = new BABYLON.StandardMaterial("boxMaterail", scene);
@@ -147,39 +148,117 @@ export class NormalScene implements CreateSceneClass {
         //radius around the analyzed pixel. Default: 0.0006
         var radius = 0.006;
         // Default: 0.0075;
-        var area = 0.0075;
+        var area = 0.02;
         var fallOff = 0.000001;
         //base color of SSAO
         var base = 0.5;
         //max value of SSAO
         var totalStrength = 1.0;
+        var kernelSphereData2: number[] = [];
         for (let index = 0; index < numSamples; index++) {
             var sample = new BABYLON.Vector3(
                 Math.random() * 2.0 - 1.0,
                 Math.random() * 2.0 - 1.0,
-                Math.random());
+                - Math.random());
             sample.normalize();
             var scale = index / numSamples;
             //improve distribution
             scale = BABYLON.Scalar.Lerp(0.1, 1.0, scale * scale);
             sample.scale(scale);
+            //TEST
+            sample.x = +sample.x.toFixed(3);
+            sample.y = +sample.y.toFixed(3);
+            sample.z = +sample.z.toFixed(3);
             //sample.scale(BABYLON.Scalar.RandomRange(0, 1));
+            console.log(sample.toString())
             kernelSphere.push(sample);
+            kernelSphereData2.push(parseFloat(sample.x.toString()));
+            kernelSphereData2.push(parseFloat(sample.y.toString()));
+            kernelSphereData2.push(parseFloat(sample.z.toString()));
         }
         var kernelSphereData = kernelSphere.data.map(Number);
+        
+        
+        kernelSphereData.forEach(element => {
+           console.log(element.toString()); 
+        });
+        console.log(kernelSphereData2.toString());
+        
+        
         //noise texture
-        var noiseTexture = this._generateNoiseTexture(scene);
+        // var noiseTexture = this._generateNoiseTexture(scene);
+        // var noiseTexture = new BABYLON.Texture("https://imgur.com/BJwNGuo", scene);
+        var noiseTexture = new BABYLON.Texture("WhiteNoiseDithering.png", scene);
+        noiseTexture.wrapU = BABYLON.Texture.WRAP_ADDRESSMODE;
+        noiseTexture.wrapV = BABYLON.Texture.WRAP_ADDRESSMODE;
+        var depthTexture = scene.enableDepthRenderer(camera, false).getDepthMap(); //false to get linear depht, true to get logarithmic depth
 
         BABYLON.Effect.ShadersStore.normalPostProcessFragmentShader = `
         precision highp float;
         
         varying vec2 vUV;
         
-        uniform sampler2D normalTexture;
         uniform sampler2D textureSampler;
+        uniform sampler2D normalTexture;
+        uniform sampler2D depthTexture;
+        uniform sampler2D noiseTexture;
+
+        uniform float radius;
+        uniform int numSamples;
+        uniform vec3 kernelSphere[16]; 
+        uniform float fallOff;
+        uniform float area;
+
 
         void main(void){
+            float depth = texture2D(depthTexture, vUV).r;
+            vec3 fragPos = vec3(vUV, depth); //aint this screen coordinates?
+            vec3 fragN = texture2D(normalTexture, vUV).xyz;
+            vec3 randomVec = normalize(texture2D(noiseTexture, vUV).rgb);
+
+            //cant remember why
+            float scale = radius / depth;
+            scale = radius; 
+
+            float ao = 0.0;
+            for(int i = 0; i < numSamples; i++){
+                //REDO THIS PART
+                // vec3 samplePosition = scale * reflect(kernelSphere[i], randomVec);
+                // vec3 hemiRay = fragPos + sign(dot(samplePosition, fragN)) * samplePosition;
+                // float occlusionDepth = texture2D(textureSampler, clamp(hemiRay.xy, vec2(0.001, 0.001), vec2(0.999, 0.999))).r;
+                // float difference = depth - occlusionDepth;
+                
+                // ao += step(fallOff, difference) * (1.0 - smoothstep(fallOff, area, difference));
+                
+                //reflect(I, N) = returns the reflection direction calculated as I - 2.0 * dot(N, I) * N. 
+                // vec3 samplePosition = reflect(randomVec, kernelSphere[i]) * scale;
+                
+                // vec3 samplePosition = fragPos + kernelSphere[i] * sign(dot(fragN, kernelSphere[i]));
+
+
+                
+                vec3 samplePosition = fragPos + kernelSphere[i] * scale;
+                
+
+                float sampleDepth = samplePosition.z;
+                float actualDepth = texture2D(depthTexture, samplePosition.xy).r;
+                float difference = actualDepth - sampleDepth;
+
+                float rangeCheck =  smoothstep(0.0001, 1.0, radius / abs(difference));
+
+                rangeCheck = abs(actualDepth - depth) < area ? 1.0 : 0.0;
+
+                ao += difference > 0.0001 ? 0.0 : 1.0 * rangeCheck;
+            }
+            ao /= float(numSamples);
+            ao = 1.0 - ao;
             gl_FragColor = texture2D(normalTexture, vUV);
+            // gl_FragColor = texture2D(depthTexture, vUV);
+            // gl_FragColor = texture2D(noiseTexture, vUV);
+            gl_FragColor = vec4(ao, ao, ao, 1);
+            // float result = floor(vUV.x * 1000.0 / float(numSamples));
+            // float algo = float(numSamples) - result * vUV.x;
+            // gl_FragColor = vec4(kernelSphere[ int(algo) ], 1);
             // gl_FragColor = texture2D(textureSampler, vUV);
         }
 
@@ -189,8 +268,8 @@ export class NormalScene implements CreateSceneClass {
         var normalPostProcessPass = new BABYLON.PostProcess(
             'Normal Post Process shader',
             'normalPostProcess',
-            null,
-            ['normalTexture'],
+            ['radius', 'numSamples', 'kernelSphere', 'fallOff', 'area'],
+            ['normalTexture', 'depthTexture', 'noiseTexture'],
             1.0,
             camera,
             BABYLON.Texture.BILINEAR_SAMPLINGMODE,
@@ -198,7 +277,15 @@ export class NormalScene implements CreateSceneClass {
         );
 
         normalPostProcessPass.onApply = function (effect) {
+            effect.setFloat("radius", radius);
+            effect.setInt("numSamples", numSamples);
+            effect.setArray3("kernelSphere", kernelSphereData2);
+            effect.setFloat("fallOff", fallOff);
+            effect.setFloat("area", area);
+
             effect.setTexture("normalTexture", normalRenderTarget);
+            effect.setTexture("depthTexture", depthTexture);
+            effect.setTexture("noiseTexture", noiseTexture);
         }
 
         
@@ -212,23 +299,24 @@ export class NormalScene implements CreateSceneClass {
     private _generateNoiseTexture(scene: BABYLON.Scene): BABYLON.DynamicTexture {
         var size = 512;
         var noiseTexture = new BABYLON.DynamicTexture("noiseTexture", size, scene, false, BABYLON.Texture.TRILINEAR_SAMPLINGMODE);
-        noiseTexture.wrapU = BABYLON.Texture.WRAP_ADDRESSMODE;
-        noiseTexture.wrapV = BABYLON.Texture.WRAP_ADDRESSMODE;
-
+        
         var context = noiseTexture.getContext();
-
+        
         var noiseVector = BABYLON.Vector3.Zero();
         for (let x = 0; x < size; x++) {
             for (let y = 0; y < size; y++) {
                 noiseVector.x = Math.floor((Math.random() * 2.0 - 1.0) * 255);
                 noiseVector.y = Math.floor((Math.random() * 2.0 - 1.0) * 255);
                 noiseVector.z = Math.floor((Math.random() * 2.0 - 1.0) * 255);
+                noiseVector = new BABYLON.Vector3(0,0,0);
 
                 context.fillStyle = 'rgb(' + noiseVector.x + ', ' + noiseVector.y + ', ' + noiseVector.z + ')';
                 context.fillRect(x, y, 1, 1);
             }
         }
-
+        
+        noiseTexture.wrapU = BABYLON.Texture.WRAP_ADDRESSMODE;
+        noiseTexture.wrapV = BABYLON.Texture.WRAP_ADDRESSMODE;
         return noiseTexture;
     }
 }
